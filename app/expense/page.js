a4851b3decd6497761cc34c47df148864e32fc4a
Aman <aman.kumar@criodo.com>
2024-09-01 23:31:44 +0530

    Added expense chart

---
+++ app/expense/page.js
@@ -0,0 +1,9 @@
+import ExpensesChart from "@/components/Expenses/ExpensesChart";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-50">
+      <ExpensesChart />
+    </div>
+  );
+}
